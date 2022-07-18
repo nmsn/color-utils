66d484d56461d7e5b7c85bf9df28df2c5d1c5115
nmsn <136696700@qq.com>
2022-07-18 17:48:04 +0800

    test(test): init test

---
+++ __tests__/transform.test.ts
@@ -0,0 +1,9 @@
+// import { mix2Color, color2Color } from "../transform";
+
+// describe("test transform functions", () => {
+//   it("mix2Color", () => {
+//     expect(mix2Color(["white", "black"], "rgb")).toBe('');
+//     expect(mix2Color(["white", "black"], "rgb")).toBe('');
+//     expect(mix2Color(["white", "black"], "rgb")).toBe('');
+//   });
+// });
---
+++ __tests__/validator.test.ts
@@ -0,0 +1,29 @@
+import { isColorName, isHex, isRgb, isRgba, isColor } from "../validator";
+
+describe("test validator functions", () => {
+  it("param is valid color name", () => {
+    expect(isColorName("white")).toBeTruthy();
+  });
+
+  it("param is valid color hex", () => {
+    expect(isHex("#fff")).toBeTruthy();
+    expect(isHex("#000000")).toBeTruthy();
+  });
+
+  it("param is valid rgb string", () => {
+    expect(isRgb("rgb(255,255,255)")).toBeTruthy();
+    expect(isRgb("rgb(0, 0, 0)")).toBeTruthy();
+  });
+
+  it("param is valid rgba string", () => {
+    expect(isRgba("rgba(255,255,255,1)")).toBeTruthy();
+    expect(isRgba("rgba(0, 0, 0, 1)")).toBeTruthy();
+  });
+
+  it("param is valid color", () => {
+    expect(isColor("white")).toBeTruthy();
+    expect(isColor("#000000")).toBeTruthy();
+    expect(isColor("rgb(255, 255, 255)")).toBeTruthy();
+    expect(isColor("rgba(255, 255, 255, 1)")).toBeTruthy();
+  });
+});
