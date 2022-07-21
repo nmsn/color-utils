2bb90b809166e67813f49692be72a8e3b36e53ae
nmsn <136696700@qq.com>
2022-07-21 21:19:04 +0800

    build(rollup): add rollup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+// rollup.config.js
+import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: 'dist/bundles.js',
+    format: "esm",
+  },
+  plugins: [typescript(), commonjs(), nodeResolve()],
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { mix2Color, color2Color } from "./transform";
+import * as validator from "./validator";
+
+export { mix2Color, color2Color, validator };
