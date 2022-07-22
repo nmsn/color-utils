5aaf7c5c174b29934b031f2533fc6e141ef4af68
nmsn <136696700@qq.com>
2022-07-22 17:21:28 +0800

    build(rollup): update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 // rollup.config.js
 import typescript from "@rollup/plugin-typescript";
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/index.ts",
   output: {
-    file: 'dist/bundles.js',
+    file: "dist/bundles.js",
     format: "esm",
+    sourcemap: true,
   },
   plugins: [typescript(), commonjs(), nodeResolve()],
 };
