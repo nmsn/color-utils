1f8798ff05eae79857fc850ad639b23941301d1a
nmsn <136696700@qq.com>
2022-09-07 16:28:08 +0800

    feat(helper): add getColorType func

--- src/__tests__/helper.test.ts
+++ src/__tests__/helper.test.ts
@@ -1,7 +1,17 @@
-import { removeRgbaBlank } from "../utils/helper";
+import { removeRgbaBlank, getColorType } from "../utils/helper";
 
 describe("helper", () => {
   it("helper", () => {
     expect(removeRgbaBlank('rgba(0, 0, 0, 1)')).toBe('rgba(0,0,0,1)');
   });
+
+  it("getColorType", () => {
+    expect(getColorType("rgba(255,255,255,1)")).toBe('rgba');
+    expect(getColorType("rgba(0,0,0,0)")).toBe('rgba');
+    expect(getColorType("rgb(255,255,255)")).toBe('rgb');
+    expect(getColorType("rgb(0,0,0)")).toBe('rgb');
+    expect(getColorType('#ffffffff')).toBe('hex');
+    expect(getColorType('#ffffff')).toBe('hex');
+    expect(getColorType('#fff')).toBe('hex');
+  })
 });
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,13 @@
+import { isHex, isRgba, isRgb } from "./validator";
+
 export const removeRgbaBlank = (color: string) => {
   return color.replace(/\s+/g, "");
+};
+
+export const getColorType = (color: string) => {
+  if (isHex(color)) return "hex";
+  if (isRgb(color)) return "rgb";
+  if (isRgba(color)) return "rgba";
+
+  return "";
 };
